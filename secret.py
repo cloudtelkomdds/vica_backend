baef3a273e32a2992b54dd78f197e24d3e34b6c3
Yesa <8yesayesa@gmail.com>
2019-12-06 16:34:49 +0700

    Delete secret.py

--- secret.py
+++
@@ -1,8 +0,0 @@
-#TODO: Move secrets into external file
-class Secret:
-    HOST = "35.186.151.35"
-    USER = "root"
-    PASSWORD = "Admintelk0m"
-    DB = "db_vica"
-
-    CLIENT_ID = "587064737111-as3mm8rh9t7hrv90deemtoijkt4st53c.apps.googleusercontent.com"
